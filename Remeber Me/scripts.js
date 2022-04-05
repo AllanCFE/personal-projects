3ea8adea1e81ca99a43c7cbcb6b80344f59799b8
AllanCFE <allan.echeverria@gmail.com>
2022-04-05 06:09:59 -0300

    Remember me: Adding new task

--- Remeber Me/scripts.js
+++ Remeber Me/scripts.js
@@ -1,1 +1,29 @@
 document.querySelector('footer').innerHTML = new Date().getHours() + ":"  + new Date().getMinutes()
+
+/*
+const interval = setInterval(function() {
+    
+}, 60000);
+*/
+
+function create () {
+    let text = document.getElementById('ttask')
+    let hour = document.getElementById('ttime')
+
+    if(text.value == "" || hour.value == "") alert('Please, insert the task and the due time')
+    else{
+        let item = document.createElement('div')
+        item.setAttribute('class', 'element')
+        item.innerHTML = `<input type='checkbox'> ${text.value} | ${hour.value}`
+        
+        text.value = ""
+        hour.value = ""
+        text.focus()
+
+        let tasklist = document.querySelector('.tasklist')
+        tasklist.appendChild(item)
+
+        if (hour.value <= new Date().getHours) console.log('oi')
+        else console.log('by')
+    }
+}
